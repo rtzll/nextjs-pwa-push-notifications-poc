e160fee6ad41787b869a27e17df1b97f0f36b219
Christian Rotzoll <ch.rotzoll@gmail.com>
2024-09-24 06:53:21 +0200

    fix: add margin to page

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import InstallPrompt from "@/components/install-prompt";
 
 export default function Page() {
   return (
-    <div className="flex flex-col gap-4 justify-center items-center h-screen">
+    <div className="flex flex-col gap-4 justify-center items-center h-screen mx-4">
       <PushNotificationManager />
       <InstallPrompt />
     </div>
